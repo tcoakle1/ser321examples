ebffe6e3890c0fa2968e5a005596c03868df0153
Tyler Coakley <tcoakle1@asu.edu>
2020-10-28 19:25:38 -0400

    Changes to WebServer for Assignment 2.

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -204,15 +204,26 @@ class WebServer {
           // extract required fields from parameters
           Integer num1 = Integer.parseInt(query_pairs.get("num1"));
           Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+          
+          	if (num2 != null && num1 != null) {
 
-          // do math
-          Integer result = num1 * num2;
+          	// do math
+          	Integer result = num1 * num2;
 
-          // Generate response
-          builder.append("HTTP/1.1 200 OK\n");
-          builder.append("Content-Type: text/html; charset=utf-8\n");
-          builder.append("\n");
-          builder.append("Result is: " + result);
+          	// Generate response
+          	builder.append("HTTP/1.1 200 OK\n");
+          	builder.append("Content-Type: text/html; charset=utf-8\n");
+          	builder.append("\n");
+          	builder.append("Result is: " + result);
+          
+          	} else {
+          	builder.append("HTTP/1.1 "); //Add rest of this line
+          	builder.append("Content-type: text/html; charset=utf-8\n");
+          	builder.append("\n");
+          	builder.append("Incorrect Syntax.\n");
+          	builder.append("");
+          
+          	}
 
           // TODO: Include error handling here with a correct error code and
           // a response that makes sense
